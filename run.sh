664ac78f6adda1f532ed06476be62fa8ad8b84bd
maximalmax90 <75565218+maximalmax90@users.noreply.github.com>
2022-12-16 16:57:09 +0800

    Entrypoint for tagged-version

--- run.sh
+++ run.sh
@@ -1,35 +1,16 @@
 #!/bin/bash
-echo "Checking existing server file..."
-if [ ! -f "/HKMP/HKMPServer.exe" ]; then
-        echo "EXE-file not found, copy..."
-        cp /FILES/HKMPServer.exe /HKMP/HKMPServer.exe;
-else
-        echo "EXE-file found! Compare checksum..."
-        if [ `md5sum /FILES/HKMPServer.exe | awk '{print $1}'` != `md5sum /HKMP/HKMPServer.exe | awk '{print $1}'` ]; then
-                echo "Checksum mismatch, updating file..."
-                cp /FILES/HKMPServer.exe /HKMP/HKMPServer.exe;
-        else
-                echo "Matched!"
-        fi
-fi
-if [ ! -f "/HKMP/HKMPServer.pdb" ]; then
-        echo "PDB-file not found, copy..."
-        cp /FILES/HKMPServer.pdb /HKMP/HKMPServer.pdb;
-else
-        echo "PDB-file found! Compare checksum..."
-        if [ `md5sum /FILES/HKMPServer.pdb | awk '{print $1}'` != `md5sum /HKMP/HKMPServer.pdb | awk '{print $1}'` ]; then
-                echo "Checksum mismatch, updating file..."
-                cp /FILES/HKMPServer.pdb /HKMP/HKMPServer.pdb;
-        else
-                echo "Matched!"
-        fi
-fi
-echo "Staring server in screen. To attach: screen -x. To detach: ctrl+a d"
-screen -d -m -S server mono /HKMP/HKMPServer.exe 2222
-while true; do
-        sleep 5;
-        if [ `ps | grep mono -c` -le 1 ]; then
-                echo "Server stopped. Exiting..."
-                exit 0
-        fi
-done
+SESSION="server"
+LOG="logs/server.log"
+EXE="HKMPServer.exe"
+PDB="HKMPServer.pdb"
+DIR="/HKMP"
+TMPDIR="/FILES"
+
+[ ! -z "$PORT" ] || PORT=2222;
+
+[ ! -f ${DIR}/${EXE} ] && cp ${TMPDIR}/${EXE} ${DIR}/${EXE};
+[ ! -f ${DIR}/${PDB} ] && cp ${TMPDIR}/${PDB} ${DIR}/${PDB};
+
+echo "Staring server. Use hkmp command for interact."
+screen -d -m -S ${SESSION} mono ${DIR}/${EXE} $PORT
+while true; do sleep 5; [ `ps | grep mono -c` -le 1 ] && echo "Server stopped. Exiting..." && exit 0; done

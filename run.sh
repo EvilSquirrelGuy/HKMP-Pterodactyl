9b2258ee1eb88c8666d049f638ba84293db47549
maximalmax90 <75565218+maximalmax90@users.noreply.github.com>
2023-10-06 23:35:59 +0800

    Update run.sh
    Add updating main files if container has been updated
    Fix tail non-existent log

--- run.sh
+++ run.sh
@@ -8,9 +8,13 @@ TMPDIR="/FILES"
 
 [ ! -z "$PORT" ] || PORT=2222;
 
+[ -f ${DIR}/${EXE} ] && rm -f ${DIR}/${EXE};
+[ -f ${DIR}/${PDB} ] && rm -f ${DIR}/${PDB};
 [ ! -f ${DIR}/${EXE} ] && cp ${TMPDIR}/${EXE} ${DIR}/${EXE};
 [ ! -f ${DIR}/${PDB} ] && cp ${TMPDIR}/${PDB} ${DIR}/${PDB};
 
 echo "Staring server. Use hkmp command for interact."
 screen -d -m -S ${SESSION} mono ${DIR}/${EXE} $PORT
+sleep 3
+tail -f $LOG &
 while true; do sleep 5; [ `ps | grep mono -c` -le 1 ] && echo "Server stopped. Exiting..." && exit 0; done
